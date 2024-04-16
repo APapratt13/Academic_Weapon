c75ab780fd9e9723886e217458677689e8fb322b
Papratt <159354536+APapratt13@users.noreply.github.com>
2024-04-16 21:52:49 +0200

    OOP lab2
    Dino

---
+++ #2sem/OOP/Labos02/Point, Shape, Circle, Rectangle.../Point.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+public class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public static double distanceTo(Point p1, Point p2){
+        return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    }
+}
